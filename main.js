32443d1b5d65b840cbf977edfa0389b0006df7aa
Lauprof <92731572+Lauprof@users.noreply.github.com>
2023-04-10 18:47:34 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,31 @@
+
+
+var canvas = document.getElementById("myCanvas");
+var ctx = canvas.getContext("2d");
+ctx.font = "30px Comic Sans MS";
+ctx.fillStyle = "red";
+ctx.textAlign = "center";
+ctx.fillText("BetSpot", canvas.width/2, canvas.height/2);
+
+
+var text ="Continue betting with us";
+var output = document.getElementById("welcome");
+var i = 0;
+function writeText(){
+  
+  output.innerHTML= text.slice(0,i);
+  i++;
+  if(i>text.length){
+    i=0;
+  }
+  
+}
+setInterval(writeText,150);
+
+
+function validate(){
+  alert("Details Sent successfully");
+  
+  return false;
+  
+}
